e3140f820cb58803ebd98c9fe42e6f73fd1fbaba
bohort-32 <108988458+bohort-32@users.noreply.github.com>
2023-03-25 16:06:37 +0100

    Ajout menu

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import Fonctions.system as FONC_SYS
+import Fonctions.utilisateur as FONC_USR
 
 if __name__ == "__main__":
     # Chargement du fichier
     SAV = FONC_SYS.charger()
+    rep_usr = None
+
+    menu_principal = '(-1 pour stopper)\n1 - Test\n2 - Salut\n\n Choix : '
+
+    while rep_usr != -1:
+        try:
+            rep_usr = int(input(menu_principal))
+        except:
+            rep_usr = None
+        if rep_usr == 1 or rep_usr == 3:
+            print(rep_usr)
+        elif rep_usr != -1:
+            print('Mauvaise saisie')
